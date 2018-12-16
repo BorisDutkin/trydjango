398dca27161bba69d13da99bbe4038c48b0971f3
boris.d <boris.dutkin@gmail.com>
2018-12-16 09:25:04 +0200

    return html string, with html variables

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,5 +4,21 @@ from django.shortcuts import render
 
 # function based view
 def home(request):
-    return HttpResponse('hello')
+
+    html_var = 'f strings'
+
+    html_ = f"""
+        <html lang=en>
+        <head>
+        </head>
+        
+        <body>
+            <h1>Hello World</h1>
+            <p>This is {html_var} coming through</p>
+        </body>
+        
+        </html>
+    """
+
+    return HttpResponse(html_)
     #return render(request, 'home.html', {})

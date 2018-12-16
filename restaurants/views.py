c64bf559898e395f4e01fd83746314fd5f98014e
boris.d <boris.dutkin@gmail.com>
2018-12-16 17:13:06 +0200

    update views.py (home / about / contact) create templates for each view create base.html + inheritance update urls.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -18,7 +18,50 @@ def home(request):
     if condition_bool_item:
         num = random.randint(0, 1000000)
 
-    return render(request, 'base.html', {
+    return render(request, 'home.html', {
         'num': num,
         'some_list': some_list
     })
+
+
+def about(request):
+
+    num = None
+
+    some_list = [
+        random.randint(0, 1000000),
+        random.randint(0, 1000000),
+        random.randint(0, 1000000)
+    ]
+
+    condition_bool_item = True
+
+    if condition_bool_item:
+        num = random.randint(0, 1000000)
+
+    return render(request, 'about.html', {
+        'num': num,
+        'some_list': some_list
+    })
+
+
+def contact(request):
+
+    num = None
+
+    some_list = [
+        random.randint(0, 1000000),
+        random.randint(0, 1000000),
+        random.randint(0, 1000000)
+    ]
+
+    condition_bool_item = True
+
+    if condition_bool_item:
+        num = random.randint(0, 1000000)
+
+    return render(request, 'contact.html', {
+        'num': num,
+        'some_list': some_list
+    })
+
